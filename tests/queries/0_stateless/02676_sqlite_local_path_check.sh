d550fb5f41a042e1488614d748832a37e1c735f8
Nikolay Degterinsky <evillique@gmail.com>
2023-03-15 10:43:10 +0000

    Fix test

--- tests/queries/0_stateless/02676_sqlite_local_path_check.sh
+++ tests/queries/0_stateless/02676_sqlite_local_path_check.sh
@@ -5,4 +5,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_LOCAL} -q "SELECT * FROM sqlite('/nonexistent', 'table')" 2>&1 | grep -c "PATH_ACCESS_DENIED";
+${CLICKHOUSE_LOCAL} -q "SELECT * FROM sqlite('/nonexistent', 'table')" 2>&1 | grep -c "unable to open database file";
